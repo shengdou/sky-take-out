e4cdb7f7f5c90656dd52b4ca607aa08061538345
Moyu <2983188001@qq.com>
2024-06-09 12:25:46 +0800

    查看、删除、清空购物车业务代码

--- sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
+++ sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
@@ -1,16 +1,17 @@
 package com.sky.controller.user;
 
 import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
 import com.sky.result.Result;
 import com.sky.service.ShoppingCartService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.annotations.Delete;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author Moyu
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @description: 购物车相关接口
  * @date 2024/5/22 0:15
  */
+//TODO  设计触发器保持购物车数据一致性
 @RestController
 @RequestMapping("/user/shoppingCart")
 @Api(tags = "C端用户购物车相关接口")
@@ -31,6 +33,33 @@ public class ShoppingCartController {
     @ApiOperation("添加购物车")
     public Result add(@RequestBody ShoppingCartDTO shoppingCartDTO) {
         shoppingCartService.addShoppingCart(shoppingCartDTO);
+        return Result.success();
+    }
+
+    @GetMapping("/list")
+    @ApiOperation("查看购物车")
+    public Result<List<ShoppingCart>> list() {
+        List<ShoppingCart> carts=shoppingCartService.list();
+        return Result.success(carts);
+    }
+
+    @DeleteMapping("/clean")
+    @ApiOperation("清空购物车")
+    public Result clean() {
+        shoppingCartService.clear();
+        return Result.success();
+    }
+
+    /**
+     * 删除购物车中一个商品
+     * @param shoppingCartDTO
+     * @return
+     */
+    @PostMapping("/sub")
+    @ApiOperation("删除购物车中一个商品")
+    public Result sub(@RequestBody ShoppingCartDTO shoppingCartDTO){
+        log.info("删除购物车中一个商品，商品：{}", shoppingCartDTO);
+        shoppingCartService.subShoppingCart(shoppingCartDTO);
         return Result.success();
     }
 }
--- sky-server/src/main/java/com/sky/service/ShoppingCartService.java
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -3,6 +3,8 @@ package com.sky.service;
 import com.sky.dto.ShoppingCartDTO;
 import com.sky.entity.ShoppingCart;
 
+import java.util.List;
+
 /**
  * @author Moyu
  * @version 1.0
@@ -13,4 +15,13 @@ public interface ShoppingCartService {
 
     public void addShoppingCart(ShoppingCartDTO shoppingCartDTO);
 
+    List<ShoppingCart> list();
+
+    void clear();
+
+    /**
+     * 删除购物车中一个商品
+     * @param shoppingCartDTO
+     */
+    void subShoppingCart(ShoppingCartDTO shoppingCartDTO);
 }
